70c0e09a0b69048caa30b7932d430473f0b5157a
thegetoutgirl <amyahaha@gmail.com>
2020-04-08 23:05:48 -0400

    updated notes routes

---
+++ noteserver.js
@@ -0,0 +1,20 @@
+// BACK END PACKAGES
+const http = require("http");
+const fs = require ("fs");
+const uuid = require("uuid");
+const express = require("express") 
+
+// PORT & SERVER
+const PORT = 7017;
+// const server = http.createServer(); not sure i need something here?
+
+// GET ROUTE
+
+// POST ROUTE
+
+// DELETE ROUTE
+
+// APP.LISTEN
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+})
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // nick's code
-
 const express = require("express");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -11,12 +10,25 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const db = require(".db/db.json");
+// const db = require(".db/db.json");
 
+// Returns notes.html file
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html")    )
   });
 
+  // Returns index.html page
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+// GET Reads the db.json file and returns all saved notes as JSON
+
+// app.get("api/notes", function(req, res){
+
+// })
+
+// POST Receives a new note to save on the request body, add to JSON file, return new note to client
 app.post("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./db.json"));
 //   // Add res.params.id after the app.delete()
@@ -24,9 +36,12 @@ app.post("/api/notes", function(req, res) {
   //   // push note to array
   //   // fs.writeFile or appendFile to save the note array
 });
-app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./public/index.html"));
-    });
+
+// DELETE Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+// app.delete("/api/notes/:id", function (req, res) {
+
+// })
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
