62fe6edf3574f11cfa21c72456073011228a916d
thegetoutgirl <amyahaha@gmail.com>
2020-04-11 01:12:42 -0400

    updated delete function

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 7000;
 
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const db = require("./db/db.json");
-
-// var notesWritten {
-//   uuid
-// }
 
 // ROUTES
 
@@ -50,19 +45,35 @@ app.post("/api/notes", function(req, res) {
     
 });
 
+// UPDATE to edit a post
+// app.update("/api/notes/:id"), function (req, res) {
+//   db.findOneandUpdate(
+//     req.params.id
+//   )
+//   fs.writeFile("./db/db.json"), function() {
+//     res.json(db);
+//   }
+// }
+
 // DELETE Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-
-// app.delete("/api/notes/:id", function (req, res) {
-
-// })
+app.delete("/api/notes/:id", function (req, res) {
+  let removeId = req.params.id;
+  for (let i = 0; i < db.length; i++) {
+    if (removeId == db[i].id) {
+        db.splice(i, 1);
+    }
+  };
+    res.json(db);
+    console.log(removeId)
+  });
 
 
 // LISTENER
 
-// app.listen(process.env.PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-// })
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+})
 
-app.listen(PORT || 7000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// app.listen(PORT || 7000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
